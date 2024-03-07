773e69a5db5145bd61f6f7201467f113abbc60f2
pendara <p.ravuthpen@gmail.com>
2024-03-07 13:17:18 +0700

    add page admin page mainpage

---
+++ lib/page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyOage extends StatefulWidget {
+  const MyOage({super.key});
+
+  @override
+  State<MyOage> createState() => _MyOageState();
+}
+
+class _MyOageState extends State<MyOage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
